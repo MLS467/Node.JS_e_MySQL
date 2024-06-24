fc2cd6a53caf869c9c142e24f0d5c96b33e661d8
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2024-06-24 12:27:12 -0300

    adc o boostrap

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// FAZENDO IMPORTAÇÕES DOS MÓDULOS
 const express = require('express')
 const app = express();
 const mysql = require('mysql2');
@@ -5,6 +6,7 @@ const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const porta = process.env.PORT || 8080;
 
+// FAZENDO A CONEXÃO COM O BANCO DE DADOS
 const conexao = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -17,17 +19,23 @@ conexao.connect((erro) => {
     console.log("Conectado com sucesso!");
 })
 
-app.use(express.static('./public'))
+// CONFIGURAÇÃO DO PUBLIC E DO BOOTSTRAP
+app.use(express.static('./public'));
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
 
+// CONFIGURAÇÃO DO BODY PARSER
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// CONFIGURAÇÃO DO HANDLEBARS
 app.engine('hbs', hbs.engine({ defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', './views');
 
+// ROTAS
 app.get('/', (req, res) => {
     res.render('inicio')
 })
 
+// SERVIDOR
 app.listen(porta, () => console.log("Servidor rodando em http://localhost:" + porta));

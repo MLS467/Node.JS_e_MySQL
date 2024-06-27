b547018f982b75cf6610c1969cca2f0bfc7b21f4
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2024-06-27 18:33:33 -0300

    Crud completo

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const file = require('express-fileupload');
 const mysql = require('mysql2');
 const hbs = require('express-handlebars');
+const fs = require('fs');
 const bodyParser = require('body-parser');
+const { error } = require('console');
 const porta = process.env.PORT || 8080;
 
 // FAZENDO A CONEXÃO COM O BANCO DE DADOS
@@ -19,8 +21,8 @@ conexao.connect((erro) => {
     if (erro) throw erro;
     console.log("Conectado com sucesso!");
 });
-// CONFIGURANDO O FILEUPLOAD
 
+// CONFIGURANDO O FILEUPLOAD
 app.use(file());
 
 // CONFIGURAÇÃO DO PUBLIC E DO BOOTSTRAP
@@ -73,5 +75,59 @@ app.post('/cadastrar', (req, res) => {
     res.redirect('/');
 })
 
-// SERVIDOR
-app.listen(porta, () => console.log("Servidor rodando em http://localhost:" + porta));
+app.get('/deletar/:cod&:imagem', (req, res) => {
+    const cod = req.params.cod;
+    const imagem = req.params.imagem;
+
+    const sql = `DELETE FROM produtos WHERE codigo_prod = ?`;
+
+    conexao.query(sql, cod, (erro, sucesso) => {
+        if (erro) throw erro;
+        console.log(sucesso);
+        fs.unlink(`${__dirname}/public/img/${imagem}`, (erro) => {
+            if (erro) throw erro;
+        });
+        console.log("Arquivo removido com sucesso!");
+    })
+    res.redirect('/');
+})
+
+
+app.get('/editar/:cod', (req, res) => {
+    console.log(req.params.cod);
+    const cod = req.params.cod;
+    const selecionar = `SELECT * FROM produtos WHERE codigo_prod = ?`;
+    conexao.query(selecionar, cod, (erro, sucesso) => {
+        if (erro) throw erro;
+        res.render('formularioEdicao', { produtos: sucesso });
+    })
+})
+
+
+app.post('/atualizar', (req, res) => {
+    const dados = {
+        codigo: req.body.codigo_prod,
+        nome: req.body.nomeProduto,
+        valor: req.body.valorProduto,
+        img: req.files.imgNome.name
+    }
+
+    const sql = `UPDATE produtos SET nome_prod=?, valor_prod = ?, imagem_prod=? WHERE codigo_prod = ?`;
+    const vet = Array(dados.nome, dados.valor, dados.img, dados.codigo);
+
+    req.files.imgNome.mv(`${__dirname}/public/img/${dados.img}`);
+
+    conexao.query(sql, vet, (error) => {
+        if (error) throw error;
+        console.log("Atualizado com sucesso!");
+        res.redirect('/');
+    })
+
+
+})
+
+// SERVIDOR 
+app.listen(
+    porta,
+    () => console.log(`Servidor rodando em http://localhost:${porta}`)
+);
